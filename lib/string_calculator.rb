d1fa64222b16ee04f4f4058b6765c4abf65f7e15
Glen Correia <glen_correia@outlook.com>
2025-09-29 14:06:59 +0530

    Write logic to add irrespective of new line character

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,7 @@ def add(number_str)
   if number_str.strip == ''
     0
   elsif number_str.include? (',')
-    numbers_array_of_strings = number_str.split(',')
+    numbers_array_of_strings = number_str.split(/,|\n/)
     numbers_array_of_integers = numbers_array_of_strings.map(&:to_i)
     total = numbers_array_of_integers.sum
   else
