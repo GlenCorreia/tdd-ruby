3654e34486c0cfe21b8eb2c09027fc5ba4ea4dd2
Glen Correia <glen_correia@outlook.com>
2025-09-29 16:05:03 +0530

    Add Rubocop to Gemfile and run it

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,30 +1,30 @@
+# frozen_string_literal: true
+
 def add(number_str)
   if number_str.strip == ''
     0
-  elsif number_str[0,2] == "//"
+  elsif number_str[0, 2] == '//'
     numbers_with_delimiter = number_str.split("\n")
     delimiter = numbers_with_delimiter[0][-1]
     string_of_numbers = numbers_with_delimiter[1]
     process_numbers(string_of_numbers, delimiter)
-  elsif number_str.include? (',')
+  elsif number_str.include?(',')
     process_numbers(number_str, /,|\n/)
   else
     number_str.to_i
   end
 end
 
-def process_numbers string_of_numbers, delimiter=','
+def process_numbers(string_of_numbers, delimiter = ',')
   numbers_array_of_strings = string_of_numbers.split(delimiter)
   numbers_array_of_integers = numbers_array_of_strings.map(&:to_i)
   numbers_array = numbers_array_of_integers.select { |number| number < 1001 } # return array of number under 1001
   negative_numbers = []
   numbers_array.each do |num|
-    if num.negative?
-      negative_numbers << num
-    end
+    negative_numbers << num if num.negative?
   end
-  if negative_numbers.length > 0
-    error_message = ""
+  if negative_numbers.length.positive?
+    error_message = ''
     if negative_numbers.length == 1
       error_message += "negatives not allowed: #{negative_numbers.first}"
     elsif negative_numbers.length > 1
@@ -33,5 +33,5 @@ def process_numbers string_of_numbers, delimiter=','
 
     raise error_message
   end
-  total = numbers_array.sum
+  numbers_array.sum
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,18 +1,20 @@
-require 'string_calculator.rb'
+# frozen_string_literal: true
 
-describe 'String Calculator: ' do 
-  it 'returns 0 for an empty string' do 
+require 'string_calculator'
+
+describe 'String Calculator: ' do
+  it 'returns 0 for an empty string' do
     expect(add('')).to eq(0)
     expect(add('   ')).to eq(0)
   end
 
-  it 'returns the single number as is' do 
+  it 'returns the single number as is' do
     expect(add('1')).to eq(1)
     expect(add('4')).to eq(4)
     expect(add('   5      ')).to eq(5)
   end
 
-  it 'adds comma seperated numbers' do 
+  it 'adds comma seperated numbers' do
     expect(add('1,5')).to eq(6)
     expect(add('1,2,3,4')).to eq(10)
     expect(add('4,4,4 ')).to eq(12)
@@ -21,25 +23,25 @@ describe 'String Calculator: ' do
     expect(add(hundred_nos)).to eq(5050)
   end
 
-  it 'allows to handle new line between numbers' do 
+  it 'allows to handle new line between numbers' do
     expect(add("1\n2,3")).to eq(6)
     expect(add("1,2\n3,4\n5,6\n7,8")).to eq(36)
   end
 
-  it 'supports different delimiters' do 
+  it 'supports different delimiters' do
     expect(add("//;\n1;2;3;4")).to eq(10)
     expect(add("//-\n9-9-9")).to eq(27)
     expect(add("//>\n100>200")).to eq(300)
   end
 
-  it 'raises exception when negative number is passed' do 
-    expect{add('-1,3,4')}.to raise_error(RuntimeError, "negatives not allowed: -1")
-    expect{add('45,-67,9')}.to raise_error(RuntimeError, "negatives not allowed: -67")
-    expect{add('-1,2,-33,5')}.to raise_error(RuntimeError, "negatives not allowed: [-1, -33]")
-    expect{add('-1,99,-999,-9999')}.to raise_error(RuntimeError, "negatives not allowed: [-1, -999, -9999]")
+  it 'raises exception when negative number is passed' do
+    expect { add('-1,3,4') }.to raise_error(RuntimeError, 'negatives not allowed: -1')
+    expect { add('45,-67,9') }.to raise_error(RuntimeError, 'negatives not allowed: -67')
+    expect { add('-1,2,-33,5') }.to raise_error(RuntimeError, 'negatives not allowed: [-1, -33]')
+    expect { add('-1,99,-999,-9999') }.to raise_error(RuntimeError, 'negatives not allowed: [-1, -999, -9999]')
   end
 
-  it 'ignores numbers greater than 1000' do 
+  it 'ignores numbers greater than 1000' do
     expect(add('2,1001')).to eq(2)
     expect(add('9090,7,10,1434,13,10,111001')).to eq(40)
   end
