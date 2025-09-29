0930c1f8d98e39d9ad62e07990af480798c5ded6
Glen Correia <glen_correia@outlook.com>
2025-09-29 14:40:52 +0530

    Refactor code - DRYing out common functionality, thereby making it readable

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -4,15 +4,17 @@ def add(number_str)
   elsif number_str[0,2] == "//"
     numbers_with_delimiter = number_str.split("\n")
     delimiter = numbers_with_delimiter[0][-1]
-    array = numbers_with_delimiter[1]
-    numbers_array_of_strings = number_str.split(delimiter)
-    numbers_array_of_integers = numbers_array_of_strings.map(&:to_i)
-    total = numbers_array_of_integers.sum
+    string_of_numbers = numbers_with_delimiter[1]
+    process_numbers(string_of_numbers, delimiter)
   elsif number_str.include? (',')
-    numbers_array_of_strings = number_str.split(/,|\n/)
-    numbers_array_of_integers = numbers_array_of_strings.map(&:to_i)
-    total = numbers_array_of_integers.sum
+    process_numbers(number_str, /,|\n/)
   else
     number_str.to_i
   end
+end
+
+def process_numbers string_of_numbers, delimiter=','
+  numbers_array_of_strings = string_of_numbers.split(delimiter)
+  numbers_array_of_integers = numbers_array_of_strings.map(&:to_i)
+  total = numbers_array_of_integers.sum
 end
