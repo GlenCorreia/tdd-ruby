3fcc22e7acb44831a8730ad9f35353e81332dd1d
Glen Correia <glen_correia@outlook.com>
2025-09-29 13:45:51 +0530

    Write another failing test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,5 +3,6 @@ require 'string_calculator.rb'
 describe 'String Calculator: ' do 
   it 'returns 0 for an empty string' do 
     expect(add('')).to eq(0)
+    expect(add('   ')).to eq(0)
   end
 end
