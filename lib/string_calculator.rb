c153a4febd49546c1ce11163a748101920731f99
Glen Correia <glen_correia@outlook.com>
2025-09-30 13:17:14 +0530

    Tidy up the code

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,42 +1,46 @@
 # frozen_string_literal: true
 
 def add(number_str)
-  if number_str.strip == ''
+  if number_str.strip == ''                                       # If an empty string
     0
-  elsif number_str[0, 2] == '//'
-    extracted_delimiter = extract_delimiter(number_str)
-    process_numbers(extracted_delimiter[0], extracted_delimiter[1])
-  elsif number_str.include?(',')
+  elsif number_str[0, 2] == '//'                                  # If an explicit delimiter is present
+    ext_delimiter = extract_delimiter(number_str)
+    process_numbers(ext_delimiter[0], ext_delimiter[1])
+  elsif number_str.include?(',')                                  # If a general delimiter or a newline character is present
     process_numbers(number_str, /,|\n/)
-  else
+  else                                                            # If a single number is present
     number_str.to_i
   end
 end
 
+# Method to process numbers having delimiters
 def process_numbers(string_of_numbers, delimiter = ',')
   numbers_array_of_strings = string_of_numbers.split(delimiter)
   numbers_array_of_integers = numbers_array_of_strings.map(&:to_i)
-  numbers_array = numbers_array_of_integers.select { |number| number < 1001 } # return array of number under 1001
+  numbers_array = numbers_array_of_integers.select do |number|    # Return array of number under 1001
+    number < 1001
+  end 
   negative_numbers = []
-  numbers_array.each do |num|
+  numbers_array.each do |num|                                     # Check for negative numbers
     negative_numbers << num if num.negative?
   end
   if negative_numbers.length.positive?
+    message_prefix = 'negatives not allowed:'
     error_message = ''
     if negative_numbers.length == 1
-      error_message += "negatives not allowed: #{negative_numbers.first}"
+      error_message += "#{message_prefix} #{negative_numbers.first}"
     elsif negative_numbers.length > 1
-      error_message += "negatives not allowed: #{negative_numbers}"
+      error_message += "#{message_prefix} #{negative_numbers}"
     end
-
     raise error_message
   end
-  numbers_array.sum
+  numbers_array.sum                                               # Return the sum of all the numbers of the array
 end
 
+# Method to extract delimiter
 def extract_delimiter(number_str)
-  numbers_with_delimiter = number_str.split("\n")
-  delimiter = numbers_with_delimiter[0][-1]
-  string_of_numbers = numbers_with_delimiter[1]
-  return [string_of_numbers, delimiter]
+  full_string = number_str.split("\n")
+  delimiter = full_string[0][-1]
+  string_of_numbers = full_string[1]
+  [string_of_numbers, delimiter]
 end
